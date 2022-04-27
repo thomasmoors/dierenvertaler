b9f8f9f9b36f81894e2249f3b88543b3f10b05c6
Thomas Moors <thomasmoors1992@gmail.com>
2022-04-27 12:26:26 +0200

    HTML as in example

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,51 +3,51 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Dierenvertaler</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link href="https://unpkg.com/nes.css@2.3.0/css/nes.min.css" rel="stylesheet" />
-        <style>
-            .container {
-                width: 80%;
-                margin-right: auto;
-                margin-left: auto;
-            }
-
-            .row {
-                overflow: hidden;
-            }
-
-            ._20 {
-                display: inline-block;
-                width: 20%;
-            }
-
-            ._70 {
-                display: inline-block;
-                width: 70%;
-            }
-        </style>
     </head>
-    <body class="container">
-       <textarea class="nes-textarea" name="text"></textarea>
-       <div class="row">
-           <div class="nes-select _70">
-            <select name="language">
-                <option value="detect">Taal herkennen</option>
-                @foreach(['Mens', 'Labrador', 'Poedel', 'Parkiet'] as $language)
-                    <option value="{{ Str::lower($language) }}">{{ $language }}</option>
-                @endforeach
-            </select>
+    <body>
+    <div class="container">
+        <br>
+        <div class="row">
+            <div class="col-5">
+                <label for="from">van</label>
+                <div class="nes-select">
+                    <select name="from" id="from">
+                        <option value="detect">Taal herkennen</option>
+                        @foreach(['Mens', 'Labrador', 'Poedel', 'Parkiet'] as $language)
+                            <option value="{{ Str::lower($language) }}">{{ $language }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+            <div class="col-2"></div>
+            <div class="col-5">
+                <label for="to">naar</label>
+                <div class="nes-select">
+                    <select name="to" id="to">
+                    </select>
+                </div>
+                <label>
+                    <input type="checkbox" class="nes-checkbox" checked="">
+                    <span>Ik ben zo dronken!!!</span>
+                </label>
+            </div>
         </div>
-           <button class="nes-btn is-primary _20">Vertaal</button>
-       </div>
-
-       <br><br>
-
-       @isset($output)
-       <p class="nes-balloon from-left nes-pointer">
-           {{ $output }}
-       </p>
-       @endisset
+        <br>
+        <div class="row">
+            <div class="col-5">
+                    <textarea class="nes-textarea" name="source-text"></textarea>
+            </div>
+            <div class="col-2 text-center">
+                <button class="nes-btn is-primary">Vertaal</button>
+            </div>
+            <div class="col-5">
+                <textarea class="nes-textarea" name="translated-text" disabled></textarea>
+            </div>
+        </div>
+        <br><br>
+    </div>
     </body>
 </html>
